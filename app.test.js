fdd25a509a300d2e731b8132ee6271ff20f81b8a
Gopal32 <64302429+Gopal32@users.noreply.github.com>
2024-03-08 12:14:11 +0530

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,22 +1,30 @@
 const request = require('supertest');
-const app = require('./server');
+require('dotenv').config();
+const connectDB = require('./config/db');
+let app
 let token, productId
+
+beforeAll(async () => {
+  const db = await connectDB();
+  require('./models/mongoDb').setDb(db);
+  return app = require('./server');
+})
 
 describe('User Authentication Endpoints', () => {
   // Test case for signing up a new user
   describe('POST /user/signUp', () => {
     it('should create a new user account', async () => {
       const newUser = {
-        "username": "support",
-        "password": "Support@12"
+        "username": "support12",
+        "password": "Support@122"
       };
 
       const response = await request(app)
         .post('/user/signUp')
         .send(newUser);
-      expect(response.statusCode).toBe(200);
+      expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('message', 'Success');
-      token = 'Bearer ' + response.body.token
+      token = { Authorization: 'Bearer ' + response.body.token }
     });
   });
 
@@ -24,13 +32,15 @@ describe('User Authentication Endpoints', () => {
     // Test case for signing in a new user
     it('Post /user/signIn', async () => {
       const newUser = {
-        "username": "support",
-        "password": "Support@12"
+        "username": "support12",
+        "password": "Support@122"
       };
-      const response = await  request(app)
-      .post('/user/signIn')
-      expect(response.statusCode).toBe(200);
+      const response = await request(app)
+        .post('/user/signIn')
+        .send(newUser);
+      expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('message', 'Success');
+      token = { Authorization: 'Bearer ' + response.body.token }
     })
   })
 
@@ -40,14 +50,15 @@ describe('User Authentication Endpoints', () => {
       const newUser = {
         "name": "Buy Apple Watch Series 9",
         "description": "Silver Aluminium Case with Sport Band",
-        "price":10000,
-        "stockQuantity":5
+        "price": 10000,
+        "stockQuantity": 5
       };
-      const response = await  request(app)
-      .post('/product/create').set(token)
-      expect(response.statusCode).toBe(200);
-      expect(response.body).toHaveProperty('message', 'Success');
-      productId = response.body.productId
+      const response = await request(app)
+        .post('/product/create').set(token)
+        .send(newUser);
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('message', 'Product Insert successfully');
+      productId = response.body.data.productId
     })
   })
 
@@ -57,21 +68,12 @@ describe('User Authentication Endpoints', () => {
       const newUser = {
         "name": "Buy Apple Watch Series 10",
         "description": "Silver Aluminium Case with Sport Band",
-        "price":10000,
-        "stockQuantity":5
+        "price": 10000,
+        "stockQuantity": 5
       };
-      const response = await  request(app)
-      .put(`/product/update/${productId}`).set(token)
-      expect(response.statusCode).toBe(200);
-      expect(response.body).toHaveProperty('message', 'Product Insert successfully');
-    })
-  })
-
-  describe('Product Endpoints', () => {
-    // Test case for delete product
-    it('Get /product/delete/:ProductId', async () => {
-      const response = await  request(app)
-      .get(`/product/delete/${productId}`).set(token)
+      const response = await request(app)
+        .put(`/product/update/${productId}`).set(token)
+        .send(newUser);
       expect(response.statusCode).toBe(200);
       expect(response.body).toHaveProperty('message', 'Product updated successfully');
     })
@@ -80,21 +82,30 @@ describe('User Authentication Endpoints', () => {
   describe('Product Endpoints', () => {
     // Test case for info product
     it('Get /product/info/:ProductId', async () => {
-      const response = await  request(app)
-      .get(`/product/info/${productId}`).set(token)
+      const response = await request(app)
+        .get(`/product/info/${productId}`).set(token)
       expect(response.statusCode).toBe(200);
-      expect(response.body).toHaveProperty('message', 'Product delete successfully');
+      expect(response.body).toHaveProperty('message', 'Success');
     })
   })
 
   describe('Product Endpoints', () => {
     // Test case for list product
     it('Get /product/list', async () => {
-      const response = await  request(app)
-      .get(`/product/list`).set(token)
+      const response = await request(app)
+        .get(`/product/list?limit=10&page=1`).set(token)
       expect(response.statusCode).toBe(200);
       expect(response.body).toHaveProperty('message', 'Success');
     })
   })
 
 });
+describe('Product Endpoints', () => {
+  // Test case for delete product
+  it('Get /product/delete/:ProductId', async () => {
+    const response = await request(app)
+      .delete(`/product/delete/${productId}`).set(token)
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('message', 'Product delete successfully');
+  })
+})
